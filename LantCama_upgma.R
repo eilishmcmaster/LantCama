7da93ed2ae8a944a1b1247d2c97785d10807c712
eilishmcmaster <eilish.summer@gmail.com>
2024-05-17 13:09:55 +1000

    clusters coloured by cluster

--- LantCama_upgma.R
+++ LantCama_upgma.R
@@ -14,16 +14,16 @@ m2 <- read.xlsx('/Users/eilishmcmaster/Documents/LantCama/LantCama/meta/LantCama
 x1 <- m2[m2$sample %in% ggtree_obj$data$label,]
 rownames(x1) <- x1$sample
 
-svdq_pop_colours <-   named_list_maker(x1$svdq_pop_label, 'Spectral',11)
+svdq_pop_colours <-   named_list_maker(x1$svdq_pop, 'Spectral',11)
 nation_colours <- named_list_maker(x1$national2, 'Paired',11)
-morphid_colours <- c(pink="#AA3377", PER="#228833", red="#EE6677", white="#66CCEE", orange="#CCBB44", undetermined="#2B2B2B", 'NA'="white")
+morphid_colours <- c(pink="#AA3377", PER="#228833", red="#EE6677", white="#66CCEE", orange="#CCBB44", undetermined="#2B2B2B")
 
 
 #### full tree ####
 
 ggtree_obj <- ggtree(upgma, size=0.3, layout="roundrect") %<+% x1 
 
-hmt <- gheatmap(ggtree_obj, as.matrix(x1[,c('svdq_pop_label','morphid2','national2')]),
+hmt <- gheatmap(ggtree_obj, as.matrix(x1[,c('svdq_pop','morphid2','national2')]),
                 offset=0.0006, width=.05, font.size=0,
                 colnames_angle=90, colnames_position="top",
                 custom_column_labels=c("SVDq cluster","Country"), hjust=0) +
@@ -42,7 +42,7 @@ custom_legend_theme <-   theme(legend.key.size = unit(0.5, 'lines'),
                         legend.title = element_text(size = 8),
                         legend.text = element_text(size = 6))
 # Create separate ggplots for each fill scheme
-plot_svdq_pop <- ggplot(m2, aes(x=long,y=lat, fill=svdq_pop_label)) +
+plot_svdq_pop <- ggplot(m2, aes(x=long,y=lat, fill=svdq_pop)) +
   geom_tile() +
   scale_fill_manual(name = "SVDq cluster", values = svdq_pop_colours, na.value = "grey90") +
   custom_legend_theme
@@ -84,14 +84,11 @@ ggsave("LantCama/outputs/LantCama_upgma_maf2.png",
        combined_plot, width = 25, height = 40, units = "cm", dpi=300)
 
 #### collapsed tree ####
-nodes.to.collapse <- c(1030, 1050, 873, 896, 904, 938, 953, 821, 554, 673)
 
 # Create nodes.identity as a named vector with names and values separately
-nodes.identity <- c("7", "6", "5", "4", "3", "2", "per1","1", "eacp", "eawt")
-nodes.color<- c('white', 'white', 'white', 'white', 'white', 'white', "#228833", 'white', "#AA3377", "#66CCEE")
+nodes.identity <- c("nqor", "bnor", "ghwy", "bjre", "per1_B", "per1_C", "per1_A", "red", "eacp", "eawt")
+names(nodes.identity) <- c(1030, 1050, 873, 896, 904, 938, 953, 821, 554, 673)
 
-names(nodes.identity) <- nodes.to.collapse
-names(nodes.color) <- nodes.to.collapse
 
 # # Scale clade function
 # scaleMyClade <- function(.p, .node) {
@@ -111,20 +108,21 @@ scaleMyClade <- function(.p, .node) {
 # Collapse clade function with labels and colors
 collapseMyClade <- function(.p, .node) {
   label <- nodes.identity[as.character(.node)]
-  fill_color <- nodes.color[as.character(.node)]
+  fill_color <- svdq_pop_colours[label]
   .p <- collapse(.p, .node, "max",  fill = fill_color, color="black", size=0.3)
   .p <- .p + geom_cladelabel(node = .node,align = TRUE, vjust=-0.02,offset.text=-0.0005, label = label,
                              fontsize = 2)
   return(.p)
 }
 
+nodes.to.collapse <- c(1030, 1050, 873, 896, 904, 938, 953, 821, 554, 673)
 
 # Apply scaling and collapsing to the nodes
 ggtree_obj2 <- Reduce(scaleMyClade, nodes.to.collapse, ggtree_obj)
 ggtree_obj2 <- Reduce(collapseMyClade, nodes.to.collapse, ggtree_obj2)
 
 # Add gheatmap and additional layers as required
-hmt2 <- gheatmap(ggtree_obj2, as.matrix(x1[,c('svdq_pop_label', 'morphid2','national2')]),
+hmt2 <- gheatmap(ggtree_obj2, as.matrix(x1[,c('svdq_pop', 'morphid2','national2')]),
                  offset = 0.001, width = .1, font.size = 0,
                  colnames_angle = 90, colnames_position = "top",
                  custom_column_labels = c("SVDq cluster", "Country"), hjust = 0) +
