f294955a37171541826791e807746f5eb0b4f524
eilishmcmaster <eilish.summer@gmail.com>
2024-11-28 13:16:46 +1100

    about to remove outlier score filter

--- LantCama_Nov24.R
+++ LantCama_Nov24.R
@@ -171,10 +171,6 @@ col_anno <- HeatmapAnnotation(
 ht <- Heatmap(
   d_matrix,
   name = "Distance",
-  # cluster_rows = FALSE, # Disable clustering since we are using dbscan
-  # cluster_columns = FALSE,
-  # show_row_dend = FALSE,
-  # show_column_dend = FALSE,
   clustering_method_columns = "average",
   clustering_method_rows = "average",
   top_annotation = col_anno,
@@ -184,7 +180,7 @@ ht <- Heatmap(
 )
 draw(ht, merge_legends = TRUE)
 
-###
+### TSNE ####
 
 library(Rtsne)
 
@@ -198,7 +194,7 @@ d <- dist(genotype_matrix, method = "euclidean")
 d_matrix <- as.matrix(d)
 
 # Perform t-SNE on the distance matrix (or you could use the original genotype matrix)
-tsne_result <- Rtsne(d_matrix, dims = 2, perplexity = 15, 
+tsne_result <- Rtsne(d_matrix, dims = 2, perplexity = 10, 
                      check_duplicates = FALSE, theta=0,
                      is_distance=TRUE,
                      num_threads=4)
@@ -207,57 +203,131 @@ tsne_result <- Rtsne(d_matrix, dims = 2, perplexity = 15,
 tsne_df <- data.frame(tsne_result$Y)  # Extract the 2D coordinates from t-SNE
 colnames(tsne_df) <- c("tSNE1", "tSNE2")  # Rename columns for clarity
 
+tsne_dist <- dist(tsne_result$Y) %>% as.matrix()
+
+# Extract the upper triangle
+upper_triangle <- tsne_dist[upper.tri(tsne_dist)]
+
+# Plot the histogram
+hist(upper_triangle, 
+     breaks = 100,  # Adjust number of bins as needed
+     main = "Histogram of Upper Triangle of Distance Matrix", 
+     xlab = "Pairwise Distance", 
+     col = "skyblue", 
+     border = "white")
+abline(v = 10, col = "red", lty = 2, lwd = 2)  # lty = 2 for dotted line, lwd = 2 for thickness
 
 
 # Apply HDBSCAN to the t-SNE coordinates (2D space)
-hdbscan_result <- hdbscan(tsne_df, minPts = 10)
+hdbscan_result <- hdbscan(tsne_df, minPts = 5)
+# hdbscan_result <- dbscan(tsne_df, minPts = 10, eps=4)
 
-# test <- data.frame(cluster=hdbscan_result$cluster, prob=hdbscan_result$membership_prob)
+hdb_scan_qc <- data.frame(cluster=hdbscan_result$cluster, prob=hdbscan_result$membership_prob,
+                   cd = hdbscan_result$coredist, os = hdbscan_result$outlier_scores)
+
+hdb_scan_qc_summary_stats <- hdb_scan_qc %>%
+  # filter(cluster != 0) %>%  # Exclude cluster 0
+  group_by(cluster) %>%
+  summarise(
+    mean_os = mean(os, na.rm = TRUE),  # Compute mean of outlier scores, ignoring NA values
+    sd_os = sd(os, na.rm = TRUE)       # Compute standard deviation of outlier scores
+  ) %>%
+  mutate( # Z-score indicates how many standard deviations an element is from the mean. 
+    z_score = (mean_os - mean(mean_os)) / sd(mean_os)
+  )
+
+ggplot()+
+  geom_histogram(data=hdb_scan_qc, mapping=aes(x=cd))+
+  facet_wrap(~cluster)
+
+ggplot()+
+  geom_histogram(data=hdb_scan_qc, mapping=aes(x=os))+
+  facet_wrap(~cluster)+
+  geom_vline(data = hdb_scan_qc_summary_stats, aes(xintercept = mean_os+1.5*(sd_os)), linetype = "dashed", color = "red")
+
+hdb_scan_qc_summary_stats
+
+unusually_high_clusters <- hdb_scan_qc_summary_stats %>%
+  filter(z_score > 1.5)
+
+hdbscan_result$cluster[which(hdbscan_result$cluster %in% unusually_high_clusters$cluster)] <- 0 # remove unusual clusters
 
 # Add the cluster labels to the t-SNE data
 tsne_df$db_cluster <- hdbscan_result$cluster
-# tsne_df$db_cluster[tsne_df$db_cluster == 0] <- NA  # Assign NA to noise points
+tsne_df$db_cluster[tsne_df$db_cluster == 0] <- NA  # Assign NA to noise points
 
+tsne_cols <- c(rainbow(length(unique(tsne_df$db_cluster))))
+names(tsne_cols) <- unique(tsne_df$db_cluster)
 # Plot the t-SNE result with HDBSCAN clusters
+
+hull <- tsne_df %>% 
+  filter(!is.na(db_cluster)) %>%
+  group_by(db_cluster) %>% 
+  slice(chull(tSNE1, tSNE2))
+
 ggplot(tsne_df, aes(x = tSNE1, y = tSNE2, color = factor(db_cluster))) +
+  geom_shape(data=hull, mapping=aes(group=db_cluster), color="transparent", alpha=0.1,
+             expand = 0.01, radius=0.01)+
   geom_point(alpha=0.5) +
-  scale_color_manual(values = c("gray", rainbow(length(unique(tsne_df$db_cluster))))) +
+  scale_color_manual(values = tsne_cols) +
   labs(title = "t-SNE with HDBSCAN Clusters",
        x = "t-SNE 1", y = "t-SNE 2", color = "Cluster") +
   theme_bw()
 
 
 #
+library(ComplexHeatmap)
+library(circlize)
+
+svdq_pop_colours <- colorRampPalette(c("grey20", "grey90"))(7)
+names(svdq_pop_colours) <- 1:7
+svdq_pop_colours <- c(c(eacp="#AA3377", per1="#228833",  eawt="#66CCEE"),svdq_pop_colours)
 
 clusters2 <- hdbscan_result$cluster
 
 db_df2 <- data.frame(sample=names(d),db_cluster=clusters2)
 db_df2$db_cluster[which(db_df2$db_cluster==0)] <- NA
-
+db_df2 <- merge(db_df2, m2, by='sample')
 ##
 
-library(ComplexHeatmap)
-library(circlize)
+d_matrix2 <- merge(d_matrix, db_df2, by.x=0, by.y='sample')
+rownames(d_matrix2) <- d_matrix2$Row.names
+d_matrix2$Row.names <- NULL
+d_matrix2 <- d_matrix2[match(colnames(d_matrix2)[1:nrow(d_matrix2)],rownames(d_matrix2)),]
 
-# Create a color mapping for clusters
-cluster_colors <- structure(
-  c(rainbow(max(clusters2, na.rm = TRUE))), # Gray for noise
-  names = c(seq_len(max(clusters2, na.rm = TRUE)))
-)
+tsne_cols2 <- tsne_cols[!is.na(names(tsne_cols))]
 
 # Create row and column annotations
 row_anno <- rowAnnotation(
-  db_cluster = as.factor(db_df2$db_cluster),
-  col = list(db_cluster = cluster_colors),
+  db_cluster = as.factor(d_matrix2$db_cluster),
+  col = list(db_cluster = tsne_cols2),
   annotation_legend_param = list(
     db_cluster = list(title = "DBSCAN Cluster")
   ),
   annotation_name_gp = gpar(fontsize = 0)
 )
 
+row_anno2 <- rowAnnotation(
+  Morphotype = d_matrix2$morphid2,
+  col = list(Morphotype = morphid_colours),
+  annotation_legend_param = list(
+    Morphotype = list(title = "Morphotype")
+  ),
+  annotation_name_gp = gpar(fontsize = 0)
+)
+
+row_anno3 <- rowAnnotation(
+  svdq_pop = d_matrix2$svdq_pop_label,
+  col = list(svdq_pop = svdq_pop_colours),
+  annotation_legend_param = list(
+    svdq_pop = list(title = "SVDQ pop label")
+  ),
+  annotation_name_gp = gpar(fontsize = 0)
+)
+
 col_anno <- HeatmapAnnotation(
-  db_cluster = as.factor(db_df2$db_cluster),
-  col = list(db_cluster = cluster_colors),
+  db_cluster = as.factor(d_matrix2$db_cluster),
+  col = list(db_cluster = tsne_cols2),
   annotation_legend_param = list(
     db_cluster = list(title = "DBSCAN Cluster")
   ),
@@ -266,12 +336,12 @@ col_anno <- HeatmapAnnotation(
 
 # Create the heatmap
 ht <- Heatmap(
-  d_matrix,
+  d_matrix2[,1:nrow(d_matrix2)] %>% as.matrix,
   name = "Distance",
   clustering_method_columns = "average",
   clustering_method_rows = "average",
   top_annotation = col_anno,
-  right_annotation = row_anno,
+  right_annotation = c(row_anno, row_anno2,row_anno3),
   show_column_names = FALSE,  # Remove column names
   show_row_names = FALSE,   
   col = colorRamp2(c(min(d_matrix), max(d_matrix)), c("white", "blue"))
